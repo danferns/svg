991d7bb8d184e51bcf4e072c1f4fe16fb3848ffb
dannywritescode <57069381+dannywritescode@users.noreply.github.com>
2022-04-18 17:59:23 +0530

    add bezier curve

---
+++ interactive/bezier/App.svelte
@@ -0,0 +1,32 @@
+<script>
+    import ControlPoint from "./ControlPoint.svelte";
+
+    let x1 = 10,
+        y1 = 200,
+        x2 = 300,
+        y2 = 100,
+        x3 = 50,
+        y3 = 60;
+</script>
+
+<svg>
+    <path d="M{x1} {y1} C {x1} {y1} {x3} {y3} {x2} {y2}" />
+
+    <ControlPoint bind:x={x1} bind:y={y1} />
+    <ControlPoint bind:x={x2} bind:y={y2} />
+    <ControlPoint bind:x={x3} bind:y={y3} />
+</svg>
+
+<style>
+    svg {
+        height: 100%;
+        width: 100%;
+        touch-action: none;
+    }
+
+    path {
+        stroke: black;
+        stroke-width: 4px;
+        fill: none;
+    }
+</style>
---
+++ interactive/bezier/ControlPoint.svelte
@@ -0,0 +1,26 @@
+<script>
+    export let x;
+    export let y;
+
+    function handleMousedown() {
+        window.addEventListener("pointermove", handleMousemove);
+        window.addEventListener("pointerup", handleMouseup);
+    }
+    function handleMousemove(event) {
+        x += event.movementX;
+        y += event.movementY;
+    }
+
+    function handleMouseup(event) {
+        window.removeEventListener("pointermove", handleMousemove);
+        window.removeEventListener("pointerup", handleMouseup);
+    }
+</script>
+
+<circle r="10" cx={x} cy={y} on:pointerdown={handleMousedown} />
+
+<style>
+    circle:active {
+        fill: hsl(0 0% 40%);
+    }
+</style>
