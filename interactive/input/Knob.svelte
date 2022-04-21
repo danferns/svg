43ffe5f28b1ebfd90b8689e066b5852a4109f87d
dannywritescode <57069381+dannywritescode@users.noreply.github.com>
2022-04-21 22:20:19 +0530

    add notch to knob

--- interactive/input/Knob.svelte
+++ interactive/input/Knob.svelte
@@ -16,6 +16,10 @@
     $: end = polarToCartesian(x, y, meterRadius, startAngle);
     $: drawLargerArc = endAngle - startAngle > 180 ? "1" : "0";
 
+    let notchStart, notchEnd;
+    $: notchStart = polarToCartesian(x, y, meterRadius * 0.65, endAngle);
+    $: notchEnd = polarToCartesian(x, y, meterRadius * 0.825, endAngle);
+
     function boundValue(val, min, max) {
         return Math.min(Math.max(val, min), max);
     }
@@ -36,15 +40,22 @@
             percent = boundValue(percent - (50 * e.movementY) / radius, 0, 100);
         }
     }}
+    on:contextmenu|preventDefault
     height={radius * 2.4}
     width={radius * 2.4}
 >
     <circle r={radius} cx={x} cy={y} />
     <path
+        class="arc"
         d="M {start.x} {start.y} A {meterRadius} {meterRadius} 0 {drawLargerArc} 0 {end.x} {end.y}"
         stroke-width={radius / 12}
     />
     <text {x} y={y + radius * 0.15} font-size={radius * 0.5}>{Math.round(percent)}%</text>
+    <path
+        class="notch"
+        d="M {notchStart.x} {notchStart.y} L {notchEnd.x} {notchEnd.y}"
+        stroke-width={radius / 18}
+    />
 </svg>
 
 <style>
@@ -62,14 +73,14 @@
         fill: hsl(0 0% 20%);
     }
 
-    path {
+    path.arc {
         fill: none;
         stroke: hsl(180 50% 50%);
         stroke-linecap: round;
         transition: stroke 0.2s;
     }
 
-    circle:active + path {
+    circle:active + path.arc {
         stroke: hsl(180 70% 70%);
     }
 
@@ -78,5 +89,15 @@
         fill: hsl(0 0% 80%);
         text-anchor: middle;
         pointer-events: none;
+        transition: fill 0.2s;
+    }
+
+    circle:active + path.arc + text {
+        fill: white;
+    }
+
+    path.notch {
+        stroke: hsl(0 00% 50%);
+        stroke-linecap: round;
     }
 </style>
