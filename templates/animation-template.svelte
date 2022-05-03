9ca06bd039baeefd9c82da475edae7af6f77f34c
Daniel Fernandes <57069381+danferns@users.noreply.github.com>
2022-05-03 20:00:01 +0530

    Create animation-template.svelte

---
+++ templates/animation-template.svelte
@@ -0,0 +1,21 @@
+<script>
+	let time = Date.now();
+	
+	function updateTime() {
+		time = Date.now() / 1000;
+		requestAnimationFrame(updateTime);
+	}
+  
+	updateTime();
+</script>
+
+<svg>
+
+</svg>
+
+<style>
+	svg {
+		height: 100%;
+		width: 100%;
+	}
+</style>
